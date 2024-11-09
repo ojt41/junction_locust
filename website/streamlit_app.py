5a525571a0d82473ce84b6c634a93395f382b993
ojt41 <114072717+ojt41@users.noreply.github.com>
2024-11-09 19:56:19 +0200

    removed imports
    commented out import folium and strealit_folium

--- website/streamlit_app.py
+++ website/streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import folium
-from streamlit_folium import folium_static
+#import folium
+#from streamlit_folium import folium_static
 import streamlit.components.v1 as components
 
 # Configure the page
